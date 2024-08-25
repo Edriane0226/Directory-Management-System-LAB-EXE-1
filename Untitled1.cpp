7082cea146ac56b720fdd709dd59ba8be3f3cbd6
Edriane0226 <edriane.bangonon26@gmail.com>
2024-08-25 21:53:30 +0800

    FInal Update
    in int man i add char buffer[FILENAME_MAX] to holt the current directory also finishing the directory() where it change the directory using _mkdir and in change_dir() i where i used _chdir("..") to backward, and _chdir(\\) for root directory and in switch case 3:  if statement to check if changing directory is good or no and it also display the cwd.

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -9,10 +9,11 @@ void directory();
 void change_dir();
 
 int main() {
+	char buffer[FILENAME_MAX]; 
 	int choice;
 	
 	cout << "      Main Menu     " << endl;
-	cout << "_ _ _ _ _ _ _ _ _ _ _";
+	cout << "_ _ _ _ _ _ _ _ _ _ _\n";
 	cout << "1. To Display List of files\n";
 	cout << "2. To Create New Directory\n";
 	cout << "3. To Change the Working Directory\n";
@@ -20,9 +21,15 @@ int main() {
     cout << "Enter The Number: ";
 	cin >> choice;
 	
+	if(_getcwd(buffer, FILENAME_MAX)) {		
+		cout << "Current Directory: " << buffer << endl;
+	}else {
+		cout << "There is an error getting the current directory.\n";
+	}
+	
 	switch (choice) {
 		case 1:
-			list_file();
+			list_file(); 
 			break;
 		case 2:
 			directory();
@@ -58,7 +65,7 @@ void list_file() {
 	    case 2: {
 			break;
 	    	string ext;
-	    	cout << "Enter file extension: "
+	    	cout << "Enter file extension: ";
 	    	cin>> ext;
 	    	system(("dir *." + ext) .c_str());
 	    	break;
@@ -66,22 +73,71 @@ void list_file() {
 		case 3: {
 			string pattern;
 			cout << "Enter file name pattern: ";
-			cint >> pattern;
+			cin >> pattern;
 			system(("dir" + pattern) .c_str());
 			break;
 		}
 		default:
-			cout <<"Invalid choice try again.\n"
+			cout <<"Invalid choice try again.\n";
 			
 	}
 }
 
 void directory() {
-	cout << "";
+	string newDirectName;
+	cout << "Enter the Directory name: ";
+	cin >> newDirectName;
+	
+	if(_mkdir(newDirectName.c_str()) == 0) {
+		cout << newDirectName << "Directory Successfully Created";
+	}else {
+		cout << "There is an error creating new directory.";
+	}
+	
 }
 
+
 void change_dir() {
-	cout << "";
+	int choice;
+	cout << "   Change Directory   \n";
+	cout <<"_ _ _ _ _ _ _ _ _ _ _ _ _\n";
+	cout <<"1. Step by step Backward\n";
+	cout <<"2. Goto Root Directory\n";
+	cout <<"3. Forward Directory\n";
+	cout <<"Enter the Number: ";
+	cin >> choice;
+	char buffer [FILENAME_MAX];
+	switch(choice) {
+		case 1:
+			_chdir("..");
+			if(_getcwd(buffer, FILENAME_MAX)) {
+				cout << "Current Directory: " << buffer;
+			}else {
+				cout << "There is an error in changing directory";
+			}
+			break;
+		case 2:
+			_chdir("\\");
+			if(_getcwd(buffer, FILENAME_MAX)) {
+				cout << "Current Directory: " << buffer;
+			}else {
+				cout << "There is an error in changing directory";
+			}
+			break;
+		case 3: {
+			string directoryName;
+			cout << "Please enter the Directory Name: ";
+			cin >> directoryName;
+			if(_chdir(directoryName.c_str()) == 0 && _getcwd(buffer, FILENAME_MAX )) {
+				cout <<"Current Directory:  " << buffer ;
+			}else {
+				cout << "There is an error in changing directory.";
+			}
+			break;
+		}
+		deafault:
+			cout << "Invalid Choice try again.";
+	}
 }
 
 
